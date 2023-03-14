83abcd2f5e2b5caf8988ebbd1f9973ac8395b276
heung115 <heung115@naver.com>
2023-03-14 12:57:28 +0900

    change option with argparse

--- ascii.py
+++ ascii.py
@@ -1,30 +1,44 @@
 import sys
 import getopt
+import argparse
+
 
 #stringValue = sys.argv[1]
-stringValue = input()
-operator = sys.argv[1]
-inputEditValue = sys.argv[2]
-minAsciiNum = 96
-maxAsciiNum = 122
+#operator = sys.argv[1]
+#inputEditValue = sys.argv[2]
+
+parser = argparse.ArgumentParser(description='Argparse Tutorial')
+parser.add_argument('text', help="put string want to convert")
+parser.add_argument('op', help = "put operator")
+parser.add_argument('num', type=int, help = "put int circulate")
+parser.add_argument('--max',          required=False,type=int,   default=122)
+parser.add_argument('--min',          required=False,type=int,   default=96)
+args    = parser.parse_args()
+
+stringValue = args.text
+operator = args.op
+inputEditValue = args.num
+minAsciiNum = args.min 
+maxAsciiNum = args.max
 
 print("==================================================")
+
 for i in range(len(stringValue)):
-  
 #or stringValue[i]=="\'"
+#option filtering
   if(stringValue[i]==" " or stringValue[i]=="."  or stringValue[i]=="(" or stringValue[i]==")"):
     print(stringValue[i], end="")
     continue
-
+#option cycle
   match operator:
     case '+':
-      temp = ord(stringValue[i]) + int(inputEditValue)
+      temp = ord(stringValue[i]) + inputEditValue
       if temp > maxAsciiNum:
         temp = temp - maxAsciiNum + minAsciiNum
       print(chr(temp), end="")
-    case '+':
+    case '-':
       temp = ord(stringValue[i]) - int(inputEditValue)
-      if temp > maxAsciiNum:
+      if temp < minAsciiNum:
         temp = temp - maxAsciiNum + minAsciiNum
       print(chr(temp), end="")
     case '*':
